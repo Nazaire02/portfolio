2a52ea5bf31390d07b7b6004ee95a400723e1ae5
unknown <nazairendouffou@gmail.com>
2024-09-07 15:55:53 +0000

    new design

--- src/App.jsx
+++ src/App.jsx
@@ -17,29 +17,34 @@ import * as bootstrap from 'bootstrap'
 
 function App() {
   return (
-    <div className='mt-3 container-fluid'>
+    <div className='mt-3 container-fluid w-75'>
       <div className='nav-container'>
         <nav>
-          <ul className='d-flex list-unstyled'>
+          <ul className='d-flex justify-content-center list-unstyled'>
             <li><a href="#accueil">Accueil</a></li>
             <li><a href="#formations">Formations</a></li>
             <li><a href="#experience">Exprériences</a></li>
           </ul>
         </nav>
       </div>
-      <div className='row mx-3 mb-4'>
-        <div className="photo-section col-md-6 d-flex flex-column align-items-center text-center">
-          <div className="photo-container mb-4 position-relative">
-            <img src={profile} alt="Photo de N'Douffou Manda Nazaire" className="profile-photo shadow-lg" />
-            <h2 className="fw-bold fst-italic mt-3">N'DOUFFOU MANDA NAZAIRE</h2>
+      <div className='row mx-3'>
+        <div className="col-12 d-flex flex-column align-items-center text-center">
+          <div className="position-relative prentation">
+            {/* <img src={profile} alt="Photo de N'Douffou Manda Nazaire" className="profile-photo shadow-lg" /> */}
+            <p>Hey, je suis<br />
+              <span className="fw-bold">N'DOUFFOU MANDA NAZAIRE</span><br />
+              développeur web/mobile Full-Stack passionné avec une expérience dans la création d'applications web et mobiles.
+              J'ai travaillé sur divers projets en utilisant des technologies modernes telles que React, Angular, Node.js,
+              MongoDB, et bien d'autres. J'aime résoudre des problèmes complexes et créer des solutions innovantes.
+            </p>
           </div>
 
-          <div className="contact-info w-100">
-            <div className="d-flex align-items-center justify-content-center mb-3">
+          <div className="contact-info w-75 d-flex justify-content-between">
+            <div className="d-flex align-items-center justify-content-center">
               <i className="fas fa-phone contact-icon text-primary me-2"></i>
               <span className="fw-bold text-muted">+225 07 79 693 186</span>
             </div>
-            <div className="d-flex align-items-center justify-content-center mb-3">
+            <div className="d-flex align-items-center justify-content-center">
               <i className="fas fa-envelope contact-icon text-primary me-2"></i>
               <a href="mailto:email@example.com" className="fw-bold text-muted">nazairendouffou@gmail.com</a>
             </div>
@@ -50,128 +55,136 @@ function App() {
           </div>
         </div>
 
-        <div className="col-md-6 card shadow-lg border-0 p-4">
-          <div className="profile-description">
-            <h3 className='fst-italic text-primary fw-bold'>DEVELOPPEUR FULLSTACK WEB/MOBILE</h3>
-            <p>
-              Je suis un développeur Full-Stack passionné avec une expérience dans la création d'applications web et mobiles.
-              J'ai travaillé sur divers projets en utilisant des technologies modernes telles que React, Angular, Node.js,
-              MongoDB, et bien d'autres. J'aime résoudre des problèmes complexes et créer des solutions innovantes.
-            </p>
-          </div>
-
+        <div className="col-12 card shadow-lg border-0 p-4 mb-4">
           <div className="tech-stack">
-            <h4 className='fw-bold'>Technologies et outils :</h4>
+            <h4 className='fw-bold'>Technologies et outils</h4>
             <div class="row">
-              <div className="col-4 col-sm-3">
-                <img src={react} alt="React" className="tech-logo-circle" />
+              <div className="col-12 d-flex flex-row border-top border-bottom pt-3">
+                <div className='d-flex flex-column align-items-center'>
+                  <img src={react} alt="React" className="tech-logo-circle" />
+                  <p>React</p>
+                </div>
+                <div className='d-flex flex-column align-items-center'>
+                  <img src={angular} alt="Angular" className="tech-logo-circle" />
+                  <p>Angular</p>
+                </div>
+                <div className='d-flex flex-column align-items-center'>
+                  <img src={rnative} alt="React Native" className="tech-logo-circle" />
+                  <p>React Native</p>
+                </div>
               </div>
-              <div className="col-4 col-sm-3">
-                <img src={angular} alt="Angular" className="tech-logo-circle" />
+              <div className="col-12 d-flex border-bottom">
+                <div>
+                  <img src={node} alt="Node.js" className="tech-logo-circle" />
+                  <p>NodeJs</p>
+                </div>
+                <div>
+                  <img src={express} alt="Express" className="tech-logo-circle" />
+                  <p>Express</p>
+                </div>
               </div>
-              <div className="col-4 col-sm-3">
-                <img src={rnative} alt="React Native" className="tech-logo-circle" />
+              <div className="col-12 d-flex border-bottom">
+                <div>
+                  <img src={mongodb} alt="MongoDB" className="tech-logo-circle" />
+                  <p>MongoDb</p>
+                </div>
               </div>
-              <div className="col-4 col-sm-3">
-                <img src={node} alt="Node.js" className="tech-logo-circle" />
+              <div className="col-12 d-flex border-bottom">
+                <div>
+                  <img src={git} alt="Git" className="tech-logo-circle" />
+                  <p>Git</p>
+                </div>
+                <div>
+                  <img src={github} alt="GitHub" className="tech-logo-circle" />
+                  <p>GitHub</p>
+                </div>
+                <div>
+                  <img src={gitlab} alt="GitLab" className="tech-logo-circle" />
+                  <p>GitLab</p>
+                </div>
               </div>
-              <div className="col-4 col-sm-3">
-                <img src={express} alt="Express" className="tech-logo-circle" />
-              </div>
-              <div className="col-4 col-sm-3">
-                <img src={mongodb} alt="MongoDB" className="tech-logo-circle" />
-              </div>
-              <div className="col-4 col-sm-3">
-                <img src={git} alt="Git" className="tech-logo-circle" />
-              </div>
-              <div className="col-4 col-sm-3">
-                <img src={github} alt="GitHub" className="tech-logo-circle" />
-              </div>
-              <div className="col-4 col-sm-3">
-                <img src={gitlab} alt="GitLab" className="tech-logo-circle" />
-              </div>
-              <div className="col-4 col-sm-3">
-                <img src={bitbucket} alt="Bitbucket" className="tech-logo-circle" />
-              </div>
-              <div className="col-4 col-sm-3">
-                <img src={trello} alt="Trello" className="tech-logo-circle" />
-              </div>
-              <div className="col-4 col-sm-3">
-                <img src={jira} alt="Jira" className="tech-logo-circle" />
+              <div className="col-12 d-flex border-bottom">
+                <div>
+                  <img src={trello} alt="Trello" className="tech-logo-circle" />
+                  <p>Trello</p>
+                </div>
+                <div>
+                  <img src={jira} alt="Jira" className="tech-logo-circle" />
+                  <p>Jira</p>
+                </div>
               </div>
             </div>
           </div>
 
           <div className="langage-stack">
-            <h4 className='fw-bold'>Langues :</h4>
-            <ul>
-            <li>Français: Lu, écrit et parlé (langue maternelle)</li>
-            <li>Anglais: Lu, écrit et parlé (Niveau débutant)</li>
+            <h4 className='fw-bold'>Langues</h4>
+            <ul className='list-unstyled'>
+              <li>Français: Lu, écrit et parlé (langue maternelle)</li>
+              <li>Anglais: Lu, écrit et parlé (Niveau débutant)</li>
             </ul>
           </div>
         </div>
       </div>
-      <div className="row bg-light py-5">
-        <div className="col-md-6 mb-4" id="formations">
-          <h3 className="mb-4 text-primary fw-bold">Formations</h3>
-          <div className="card card-hover shadow-sm mb-3">
-            <div className="card-body">
-              <h5 className="card-title text-secondary">Master en Informatique</h5>
-              <p className="card-text">Ecole Supérieure Africaine des TIC <span className="text-muted">·</span> 2022 - 2024</p>
-              <p className="card-text fw-bold">Spécialisation : Systèmes d'information et génie logiciel.</p>
-            </div>
-          </div>
-          <div className="card card-hover shadow-sm mb-3">
-            <div className="card-body">
-              <h5 className="card-title text-secondary">Licence en Informatique</h5>
-              <p className="card-text">Ecole Supérieure Africaine des TIC <span className="text-muted">·</span> 2019 - 2024</p>
-              <p className="card-text fw-bold">Spécialisation : Systèmes et réseaux informatiques et télécommunications.</p>
-            </div>
-          </div>
-          <div className="card card-hover shadow-sm mb-3">
-            <div className="card-body">
-              <h5 className="card-title text-secondary">Baccalauréat</h5>
-              <p className="card-text">Lycée de garçons de Bingerville <span className="text-muted">·</span> 2019</p>
-              <p className="card-text fw-bold">Spécialisation : Série D (Sciences expérimentales).</p>
-            </div>
-          </div>
-        </div>
-
-        <div className="col-md-6" id="experience">
+      <div className="row py-5">
+        <div className="col-12" id="experience">
           <h3 className="mb-4 text-primary fw-bold">Expériences Professionnelles</h3>
-          <div className="card card-hover shadow-sm mb-3">
-            <div className="card-body">
-              <h5 className="card-title text-secondary">Développeur Full-Stack (En ligne)</h5>
-              <p className='card-subtitle mb-2 text-muted'>Yoomi solution <span className="text-muted">·</span> 07/2023 - Présent</p>
-              <ul className="list-unstyled fw-bold">
-                <li><i className="bi bi-check-circle me-2 text-success"></i>Développement du frontend d'une application de restauration</li>
-                <li><i className="bi bi-check-circle me-2 text-success"></i>Développement de l'API de cette application</li>
-              </ul>
+          <div className='d-flex'>
+            <div>
+              <div className="card card-hover shadow-sm mb-3 border-2">
+                <div className="card-body">
+                  <h5 className="card-title text-secondary">Développeur Full-Stack (En ligne)</h5>
+                  <p className='card-subtitle mb-2 text-muted'>Yoomi solution <span className="text-muted">·</span> 07/2023 - Présent</p>
+                  <ul className="list-unstyled fw-bold">
+                    <li><i className="bi bi-check-circle me-2 text-success"></i>Développement du frontend d'une application de restauration</li>
+                    <li><i className="bi bi-check-circle me-2 text-success"></i>Développement de l'API de cette application</li>
+                  </ul>
+                </div>
+              </div>
+              <div className="card card-hover shadow-sm mb-3 experience border-2">
+                <div className="card-body">
+                  <h5 className="card-title text-secondary">Stagiaire Développeur Full-Stack (Sur site)</h5>
+                  <p className='card-subtitle mb-2 text-muted'>Cabinet Offset Consulting <span className="text-muted">·</span>Abidjan<span className="text-muted">·</span> 04/2022 - 05/2022</p>
+                  <ul className="list-unstyled fw-bold">
+                    <li><i className="bi bi-check-circle me-2 text-success"></i>Développement d'une application de gestion de mutuelle</li>
+                  </ul>
+                </div>
+              </div>
+                            <div className="card card-hover shadow-sm mb-3 experience border-2">
+                <div className="card-body">
+                  <h5 className="card-title text-secondary">Stagiaire Développeur Full-Stack (Sur site)</h5>
+                  <p className='card-subtitle mb-2 text-muted'>Cabinet Offset Consulting <span className="text-muted">·</span>Abidjan<span className="text-muted">·</span> 04/2022 - 05/2022</p>
+                  <ul className="list-unstyled fw-bold">
+                    <li><i className="bi bi-check-circle me-2 text-success"></i>Développement d'une application de gestion de mutuelle</li>
+                  </ul>
+                </div>
+              </div>
             </div>
-          </div>
-          <div className="card card-hover shadow-sm mb-3">
-            <div className="card-body">
-              <h5 className="card-title text-secondary">Stagiaire Développeur Full-Stack (Sur site)</h5>
-              <p className='card-subtitle mb-2 text-muted'>PayQin <span className="text-muted">·</span>Abidjan<span className="text-muted">·</span> 02/2024 - 07/2024</p>
-              <ul className="list-unstyled fw-bold">
-                <li><i className="bi bi-check-circle me-2 text-success"></i>Mise à jour de l'application web et du dashboard admin</li>
-                <li><i className="bi bi-check-circle me-2 text-success"></i>Mise à jour de l'API</li>
-              </ul>
+            <div className='bar'>
             </div>
-          </div>
-          <div className="card card-hover shadow-sm mb-3">
-            <div className="card-body">
-              <h5 className="card-title text-secondary">Stagiaire Développeur Full-Stack (Sur site)</h5>
-              <p className='card-subtitle mb-2 text-muted'>Cabinet Offset Consulting <span className="text-muted">·</span>Abidjan<span className="text-muted">·</span> 04/2022 - 05/2022</p>
-              <ul className="list-unstyled fw-bold">
-                <li><i className="bi bi-check-circle me-2 text-success"></i>Développement d'une application de gestion de mutuelle</li>
-              </ul>
+            <div>
+              <div className="card card-hover shadow-sm mb-3 experience border-2">
+                <div className="card-body">
+                  <h5 className="card-title text-secondary">Stagiaire Développeur Full-Stack (Sur site)</h5>
+                  <p className='card-subtitle mb-2 text-muted'>PayQin <span className="text-muted">·</span>Abidjan<span className="text-muted">·</span> 02/2024 - 07/2024</p>
+                  <ul className="list-unstyled fw-bold">
+                    <li><i className="bi bi-check-circle me-2 text-success"></i>Mise à jour de l'application web et du dashboard admin</li>
+                    <li><i className="bi bi-check-circle me-2 text-success"></i>Mise à jour de l'API</li>
+                  </ul>
+                </div>
+              </div>
+                            <div className="card card-hover shadow-sm mb-3 experience border-2">
+                <div className="card-body">
+                  <h5 className="card-title text-secondary">Stagiaire Développeur Full-Stack (Sur site)</h5>
+                  <p className='card-subtitle mb-2 text-muted'>Cabinet Offset Consulting <span className="text-muted">·</span>Abidjan<span className="text-muted">·</span> 04/2022 - 05/2022</p>
+                  <ul className="list-unstyled fw-bold">
+                    <li><i className="bi bi-check-circle me-2 text-success"></i>Développement d'une application de gestion de mutuelle</li>
+                  </ul>
+                </div>
+              </div>
             </div>
           </div>
         </div>
       </div>
-
-
     </div>
 
   )
